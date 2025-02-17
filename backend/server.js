3aea528a595e167e2dc42bd852e59e4a97504c97
unknown <azizovagultakin@gmail.com>
2025-02-17 21:01:45 +0500

    security

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express=require("express")
 const cors=require("cors")
 const bodyParser=require("body-parser")
@@ -6,9 +8,9 @@ const app=express()
 app.use(express.json())
 app.use(cors())
 PORT=8085
-mongoose.connect('mongodb+srv://gultekin:gultekinn@cluster0.ez8varc.mongodb.net').then((res)=>{
-    console.log("connected")
-})
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("Connected to MongoDB");
+});
 const userSchema=mongoose.Schema({
     name:{
         type:String,
